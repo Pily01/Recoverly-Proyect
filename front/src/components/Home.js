6565a42ce6b1de7cf6444160bf1348dcea5e5424
Pily <paila@live.com.mx>
2019-01-09 20:12:14 -0600

    crear landing page con banner y tres cards

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -6,12 +6,35 @@ export default class Home extends Component {
     return (
       <div>
         <Navbar main="home" link1="HOME" link2="ABOUT" link3="SIGN UP" link4="LOGIN"/>
-        <div className="ui four item menu">
-          <a className="item"><img src="../../public/images/recoverly.png"/></a>
-          <a className="item">Editorials</a>
-          <a className="item">Reviews</a>
-          <a className="item active">Upcoming Events</a>
+        <div className="landing"></div>
+        <div className="container">
+
+          <div class="ui link cards container">
+            <div class="ui card">
+              <div class="image">
+                <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/50272033_2192797480984407_6758746964336050176_n.png?_nc_cat=110&_nc_ht=scontent-dfw5-1.xx&oh=fba91454f16741d2a4a75d750ebdb6ce&oe=5C8ECB95"/>
+              </div>
+            </div>
+
+            <div class="ui card">
+              <div class="image">
+                <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/49947465_2192799964317492_6515368442030194688_n.png?_nc_cat=103&_nc_ht=scontent-dfw5-1.xx&oh=a5a9b2181c0c8076f6c6669adab23a13&oe=5CCDF907"/>
+              </div>
+            </div>
+
+            <div class="ui card">
+              <div class="image">
+                <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/49762988_2192798630984292_3839138358131425280_n.png?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=26bfdfc830061c94591afbb863e20e6d&oe=5CD7F704"/>
+              </div>
+            </div>
+
+          </div>
         </div>
+        
+        
+      
+        
+        
       </div>
     )
   }

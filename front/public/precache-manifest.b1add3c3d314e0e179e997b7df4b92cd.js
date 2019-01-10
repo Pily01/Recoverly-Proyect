4809fa1f399a215c43d12ea4ae63e47e9816a8ce
Pily <paila@live.com.mx>
2019-01-09 22:26:24 -0600

    crear deploy 1

---
+++ front/public/precache-manifest.b1add3c3d314e0e179e997b7df4b92cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e705b70ea9b8a467a6a",
+    "url": "/static/js/main.6e705b70.chunk.js"
+  },
+  {
+    "revision": "1fca0b49981370f852dc",
+    "url": "/static/js/1.1fca0b49.chunk.js"
+  },
+  {
+    "revision": "6e705b70ea9b8a467a6a",
+    "url": "/static/css/main.80df377d.chunk.css"
+  },
+  {
+    "revision": "ae654929fec71be2de6aafb9b3284dc2",
+    "url": "/index.html"
+  }
+];
